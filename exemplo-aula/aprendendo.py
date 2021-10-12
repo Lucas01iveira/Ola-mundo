da218c7476c9eaa326d4429273a7cb19830a6cc6
Lucas de Paula Oliveira <87955029+Lucas01iveira@users.noreply.github.com>
2021-10-12 20:04:54 -0300

    Descrição das ferramentas

---
+++ exemplo-aula/aprendendo.py
@@ -0,0 +1,33 @@
+# Estou escrevendo esse código para testar as ferramentas do Git, GitHub e GitHub Desktop
+
+for i in range(5):
+    print('Força, meu guerreiro!')
+
+print('Atenciosamente, Lucas do passado.')
+
+print('um teste a mais')
+
+'''
+Algumas anotações sobre o funcionamento do GitHub Desktop (e do git em geral)
+
+1) Ao criar um repositório (na sua máquina, por exemplo), todas as informações, isto é, pastas, programas,
+linhas de código, etc, são automaticamente rastreadas pelo Git e apresentadas graficamente (de modo simples)
+na interface do GitHub Desktop
+
+2) Mesmo que o repositório seja criado inicialmente na plataforma online do GitHub (repositório remoto), 
+ele também pode ser transmitido para a sua máquina (repositório local) com o ícone "fetch origin" + "pull"
+do GitHub Desktop
+
+3) Após cada alteração o próprio GitHub Desktop sugere os commits a serem feitos, juntamente com todas as 
+modificações feitas. É recomendável colocar um título no commit (para saber do que se trata) e,
+se for necessário, uma breve descrição.
+
+4) Dentro da interface do GitHub Desktop, há a aba "changes" e a aba "history". É na aba "changes"
+que aparecem todas as modificações que estão sendo feitas e que podem ser commitadas. Na aba "history"
+aparecem todos os commits já feitos.
+
+5) Lembre-se de que cada commit armazena os dados apenas no repositório local da sua máquina! Para subir 
+no repositório online do GitHub é necessário fazer um "push". Mas não precisa se preocupar, pois o 
+próprio GitHub Desktop já sugere esse comando após a efetivação do commit.
+
+'''
--- exemplo-aula/brincando.py
+++
@@ -1,8 +0,0 @@
-# Estou escrevendo esse código para testar as ferramentas do Git, GitHub e GitHub Desktop
-
-for i in range(5):
-    print('Força, meu guerreiro!')
-
-print('Atenciosamente, Lucas do passado.')
-
-print('um teste a mais')
