5f71e69dd4b22204ff0eb4d0b81d40a2c6af8fd6
Lucas de Paula Oliveira <87955029+Lucas01iveira@users.noreply.github.com>
2021-10-12 19:45:09 -0300

    Criei um novo programa
    Programinha em python.

---
+++ exemplo-aula/brincando.py
@@ -0,0 +1,6 @@
+# Estou escrevendo esse código para testar as ferramentas do Git, GitHub e GitHub Desktop
+
+for i in range(5):
+    print('Força, meu guerreiro!')
+
+print('Atenciosamente, Lucas do passado.')
